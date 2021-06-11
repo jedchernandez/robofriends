72c5e9dbbf159da291f4b65c139f01ac43ce0833
Joshua Hernandez <jedchernandez@gmail.com>
2021-06-11 18:10:01 -0400

    chore: Rename template title to RoboFriends

---
+++ src/components/Scroll.js
@@ -0,0 +1,14 @@
+const Scroll = ({ children }) => {
+  return (
+    <div
+      style={{
+        overflowY: "scroll",
+        height: "500px",
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Scroll;
